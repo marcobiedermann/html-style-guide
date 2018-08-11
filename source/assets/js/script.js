3b7f154442cffdf359e112089b611f27577dd5a4
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-11 12:55:20 +0200

    fix lint errors

--- source/assets/js/script.js
+++ source/assets/js/script.js
@@ -1,13 +1,13 @@
-import hljs        from 'highlight.js/lib/highlight.js';
-import css         from 'highlight.js/lib/languages/css';
-import javascript  from 'highlight.js/lib/languages/javascript';
-import xml         from 'highlight.js/lib/languages/xml';
+import hljs from 'highlight.js/lib/highlight';
+import css from 'highlight.js/lib/languages/css';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
 
-window.ga = window.ga || function() {
-  (ga.q = ga.q || []).push(arguments)
+window.ga = window.ga || function () {
+  (ga.q = ga.q || []).push(arguments);
 };
 
-ga.l = +new Date;
+ga.l = +new Date();
 
 ga('create', 'UA-46884111-5', 'auto');
 ga('send', 'pageview');
@@ -18,16 +18,16 @@ hljs.registerLanguage('xml', xml);
 hljs.initHighlightingOnLoad();
 
 const easing = {
-  easeInOutCubic: t => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  easeInOutCubic: t => (t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1),
 };
 
 const $$sections = document.querySelectorAll('.js-section');
-const sections   = {};
+const sections = {};
 
 function scrollTo(Y, duration, easingFunction, callback) {
-  const start   = performance.now();
+  const start = performance.now();
   const element = document.documentElement.scrollTop ? document.documentElement : document.body;
-  const from    = element.scrollTop;
+  const from = element.scrollTop;
 
   if (from === Y) {
     callback();
@@ -41,41 +41,37 @@ function scrollTo(Y, duration, easingFunction, callback) {
 
   function scroll(timestamp) {
     const currentTime = performance.now();
-    const time        = min(1, ((currentTime - start) / duration));
-    const easedT      = easingFunction(time);
+    const time = min(1, ((currentTime - start) / duration));
+    const easedT = easingFunction(time);
 
     element.scrollTop = (easedT * (Y - from)) + from;
 
     if (time < 1) {
       requestAnimationFrame(scroll);
-    } else {
-
-      if (callback) {
-        callback();
-      }
-
+    } else if (callback) {
+      callback();
     }
   }
 
   requestAnimationFrame(scroll);
 }
 
-$$sections.forEach(section => {
+$$sections.forEach((section) => {
   sections[section.id] = section.offsetTop;
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', () => {
   const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
-  for (let i in sections) {
+  for (const i in sections) {
     if (sections[i] <= scrollPosition) {
       document.querySelector('.is-active').classList.remove('is-active');
       document.querySelector(`a[href="#${i}"]`).classList.add('is-active');
     }
   }
-})
+});
 
-document.querySelectorAll('.js-scroll-to').forEach(element => {
+document.querySelectorAll('.js-scroll-to').forEach((element) => {
   element.addEventListener('click', () => {
     scrollTo(document.getElementById(element.href.split('#')[1]).offsetTop, 600, easing.easeInOutCubic);
   });
