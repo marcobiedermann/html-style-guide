014b3f8ee1b9323c3890ac841c65e24338f4c5d8
Marco Biedermann <marco.biedermann@gmx.de>
2020-10-16 11:38:35 +0200

    feat: Dynamic data
    Render data dynamically

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,60 +1,32 @@
 import React, { FC } from 'react';
 import styles from './navigation.module.css';
 
-const Navigation: FC = () => {
+interface Route {
+  name: string;
+  path: string;
+}
+
+export interface NavigationProps {
+  routes: Route[];
+}
+
+const Navigation: FC<NavigationProps> = (props) => {
+  const { routes } = props;
+
   return (
     <nav className={styles.navigation}>
       <ul>
-        <li>
-          <a href="#anchor" className="is-active">
-            Anchor
-          </a>
-        </li>
-        <li>
-          <a href="#attribute">Attribute</a>
-        </li>
-        <li>
-          <a href="#comment">Comment</a>
-        </li>
-        <li>
-          <a href="#document">Document</a>
-        </li>
-        <li>
-          <a href="#form">Form</a>
-        </li>
-        <li>
-          <a href="#headline">Headline</a>
-        </li>
-        <li>
-          <a href="#image">Image</a>
-        </li>
-        <li>
-          <a href="#link">Link</a>
-        </li>
-        <li>
-          <a href="#selector">Selector</a>
-        </li>
-        <li>
-          <a href="#string">String</a>
-        </li>
-        <li>
-          <a href="#tag">Tag</a>
-        </li>
-        <li>
-          <a href="#general">General</a>
-        </li>
-        <li>
-          <a href="#css">CSS</a>
-        </li>
-        <li>
-          <a href="#javascript">JavaScript</a>
-        </li>
-        <li>
-          <a href="#tools">Tools</a>
-        </li>
-        <li>
-          <a href="#resources">Resources</a>
-        </li>
+        {routes.map((route) => {
+          const { name, path } = route;
+
+          return (
+            <li key={path}>
+              <a href={path} className="is-active">
+                {name}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
