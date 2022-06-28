e97a064240cfec82addb5b727e57ce46b76055cb
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-06-28 19:41:06 +0200

    refactor: Layout
    Refactor usage of page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import startCase from 'lodash/startCase';
 import React from 'react';
-import Aside from '../components/Aside';
 import Categories from '../components/Categories';
-import Content from '../components/Content';
-import Footer from '../components/Footer';
-import GitHub from '../components/GitHub';
-import Grid from '../components/Grid';
-import Header from '../components/Header';
-import Main from '../components/Main';
-import Navigation from '../components/Navigation';
 import Section from '../components/Section';
 import resources from '../constants/resources';
 import tools from '../constants/tools';
@@ -16,85 +7,56 @@ import { useIndexPage } from '../hooks';
 
 function IndexPage(): JSX.Element {
   const {
-    allMarkdownRemark: { distinct, group },
+    allMarkdownRemark: { group },
     site: {
-      siteMetadata: { description, github, title },
+      siteMetadata: { description, title },
     },
   } = useIndexPage();
 
   return (
     <>
-      <Header>
-        <GitHub {...github} />
-        <h1>{title}</h1>
-        <img src="/images/html5.svg" alt="HTML5 Logo" width="160" height="183" />
-      </Header>
+      <Section>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Section>
 
-      <Content>
-        <Main>
-          <Section>
-            <h2>{title}</h2>
-            <p>{description}</p>
-          </Section>
+      <Categories categories={group} />
 
-          <Categories categories={group} />
+      <Section id="tools">
+        <h2>Tools</h2>
 
-          <Section id="tools">
-            <h2>Tools</h2>
+        <ul>
+          {tools.map((tool) => {
+            const { name, url } = tool;
 
-            <ul>
-              {tools.map((tool) => {
-                const { name, url } = tool;
+            return (
+              <li key={url}>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  {name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </Section>
 
-                return (
-                  <li key={url}>
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                      {name}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </Section>
+      <Section id="resources">
+        <h2>Resources</h2>
 
-          <Section id="resources">
-            <h2>Resources</h2>
+        <ul>
+          {resources.map((resource) => {
+            const { name, url } = resource;
 
-            <ul>
-              {resources.map((resource) => {
-                const { name, url } = resource;
-
-                return (
-                  <li key={url}>
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                      {name}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </Section>
-        </Main>
-
-        <Aside>
-          <Navigation
-            routes={[...distinct, 'tools', 'resources'].map((route) => ({
-              name: startCase(route),
-              path: `#${route}`,
-            }))}
-          />
-        </Aside>
-      </Content>
-
-      <Footer>
-        <Grid>
-          <p>
-            <a href={github.url} target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </p>
-        </Grid>
-      </Footer>
+            return (
+              <li key={url}>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  {name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </Section>
     </>
   );
 }
